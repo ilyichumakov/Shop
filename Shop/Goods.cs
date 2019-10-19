9da04a05b149de9bfcf563d3dfcabbfc2081e2ad
Алексей Иксибов <student.iksib@gmail.com>
2019-10-19 09:23:03 +0300

    initial commit

---
+++ Shop/Goods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    public class Goods
+    {
+        public const int REGULAR = 0;
+        public const int SALE = 1;
+        public const int SPECIAL_OFFER = 2;
+        private String _title;
+        private int _priceCode;
+        public Goods(String title, int priceCode)
+        {
+            _title = title;
+            _priceCode = priceCode;
+        }
+        public int getPriceCode()
+        {
+            return _priceCode;
+        }
+        public void setPriceCode(int arg)
+        {
+            _priceCode = arg;
+        }
+        public String getTitle()
+        {
+            return _title;
+        }
+    }
+}
---
+++ ShopTests/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Shop;
+
+namespace ShopTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        Goods desk;
+        Goods chair;
+        Goods pencil;
+        Customer student;
+        Bill basket;
+
+        [SetUp]
+        public void CreateObjects()
+        {
+            desk = new Goods("School Desk", Goods.REGULAR);
+            chair = new Goods("Comfortable chair", Goods.SALE);
+            pencil = new Goods("Marker Blackboard", Goods.SPECIAL_OFFER);
+            student = new Customer("Chumakoff", 0);
+            basket = new Bill(student);
+            basket.addGoods(new Item(desk, 1, 65.99)); // one desk
+            basket.addGoods(new Item(chair, 4, 14.50)); // 4 chairs for student and his friends
+            basket.addGoods(new Item(pencil, 20, 0.39)); // a lot of cheap pencils
+            
+        }
+        
+
+        [Test]
+        public void GoodsTest()
+        {            
+            int type = desk.getPriceCode();
+            Assert.AreEqual(type, 0);
+        }
+
+    }
+}
