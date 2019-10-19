a75685ce779863ae336218b4804a816f71b0000b
Алексей Иксибов <student.iksib@gmail.com>
2019-10-19 10:21:32 +0300

    tests done

--- ShopTests/Class1.cs
+++ ShopTests/Class1.cs
@@ -14,18 +14,20 @@ namespace ShopTests
         Goods pencil;
         Customer student;
         Bill basket;
+        Dictionary<string, Bill> cases;
 
         [SetUp]
         public void CreateObjects()
         {
             desk = new Goods("School Desk", Goods.REGULAR);
             chair = new Goods("Comfortable chair", Goods.SALE);
-            pencil = new Goods("Marker Blackboard", Goods.SPECIAL_OFFER);
+            pencil = new Goods("Cheap pencil", Goods.SPECIAL_OFFER);
             student = new Customer("Chumakoff", 0);
             basket = new Bill(student);
-            basket.addGoods(new Item(desk, 1, 65.99)); // one desk
-            basket.addGoods(new Item(chair, 4, 14.50)); // 4 chairs for student and his friends
-            basket.addGoods(new Item(pencil, 20, 0.39)); // a lot of cheap pencils
+            //basket.addGoods(new Item(desk, 1, 65.99)); // one desk
+            //basket.addGoods(new Item(chair, 4, 14.50)); // 4 chairs for student and his friends
+            //basket.addGoods(new Item(pencil, 20, 0.39)); // a lot of cheap pencils 
+            //cases.Add("mixed", basket);
             
         }
         
@@ -35,6 +37,101 @@ namespace ShopTests
         {            
             int type = desk.getPriceCode();
             Assert.AreEqual(type, 0);
+        }
+
+        [Test]
+        public void mixedCheckoutTest()
+        {
+            basket.addGoods(new Item(desk, 1, 65.99)); // one desk
+            basket.addGoods(new Item(chair, 4, 14.50)); // 4 chairs for student and his friends
+            basket.addGoods(new Item(pencil, 20, 0.39)); // a lot of cheap pencils 
+            string actual = basket.statement();
+            string expected = "Счет для Chumakoff\n\tНазвание\tЦена\tКол-воСтоимость\tСкидка\tСумма\tБонус\n\tSchool Desk\t\t65,99\t1\t65,99\t0\t65,99\t3\n\tComfortable chair\t\t14,5\t4\t58\t0,58\t57,42\t0\n\tCheap pencil\t\t0,39\t20\t7,8\t0\t7,8\t0\nСумма счета составляет 131,21\nВы заработали 3 бонусных балов";
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void regularSingleCheckoutTest()
+        {
+            basket.addGoods(new Item(desk, 1, 65.99)); // one desk for 65.99$        
+            string actual = basket.statement();
+            string expected = "Счет для Chumakoff\n\tНазвание\tЦена\tКол-воСтоимость\tСкидка\tСумма\tБонус\n\tSchool Desk\t\t65,99\t1\t65,99\t0\t65,99\t3\nСумма счета составляет 65,99\nВы заработали 3 бонусных балов";
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void regularMultipleCheckoutTest()
+        {
+            basket.addGoods(new Item(desk, 3, 65.99)); // tree desks for 65.99$       
+            string actual = basket.statement();
+            string expected = "Счет для Chumakoff\n\tНазвание\tЦена\tКол-воСтоимость\tСкидка\tСумма\tБонус\n\tSchool Desk\t\t65,99\t3\t197,97\t5,9391\t192,0309\t9\nСумма счета составляет 192,0309\nВы заработали 9 бонусных балов";
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void saleSingleCheckoutTest()
+        {
+            basket.addGoods(new Item(chair, 1, 10)); // one chair for 10$
+            string actual = basket.statement();
+            string expected = "Счет для Chumakoff\n\tНазвание\tЦена\tКол-воСтоимость\tСкидка\tСумма\tБонус\n\tComfortable chair\t\t10\t1\t10\t0\t10\t0\nСумма счета составляет 10\nВы заработали 0 бонусных балов";
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void saleMultipleCheckoutTest()
+        {
+            basket.addGoods(new Item(chair, 5, 10)); // 5 chairs for 10$
+            string actual = basket.statement();
+            string expected = "Счет для Chumakoff\n\tНазвание\tЦена\tКол-воСтоимость\tСкидка\tСумма\tБонус\n\tComfortable chair\t\t10\t5\t50\t0,5\t49,5\t0\nСумма счета составляет 49,5\nВы заработали 0 бонусных балов";
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void saleMultipleExpensiveCheckoutTest()
+        {
+            basket.addGoods(new Item(chair, 5, 100)); // 5 chairs for 100$
+            string actual = basket.statement();
+            string expected = "Счет для Chumakoff\n\tНазвание\tЦена\tКол-воСтоимость\tСкидка\tСумма\tБонус\n\tComfortable chair\t\t100\t5\t500\t5\t495\t5\nСумма счета составляет 495\nВы заработали 5 бонусных балов";
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void SpecialCheckoutTest()
+        {
+            basket.addGoods(new Item(pencil, 5, 0.10)); // 5 pencils for 10 cents
+            string actual = basket.statement();
+            string expected = "Счет для Chumakoff\n\tНазвание\tЦена\tКол-воСтоимость\tСкидка\tСумма\tБонус\n\tCheap pencil\t\t0,1\t5\t0,5\t0\t0,5\t0\nСумма счета составляет 0,5\nВы заработали 0 бонусных балов";
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void SpecialDiscountCheckoutTest()
+        {
+            basket.addGoods(new Item(pencil, 11, 0.10)); // 5 pencils for 10 cents
+            string actual = basket.statement();
+            string expected = "Счет для Chumakoff\n\tНазвание\tЦена\tКол-воСтоимость\tСкидка\tСумма\tБонус\n\tCheap pencil\t\t0,1\t11\t1,1\t0\t1,1\t0\nСумма счета составляет 1,1\nВы заработали 0 бонусных балов";
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void SpecialExpenciveDiscountCheckoutTest()
+        {
+            basket.addGoods(new Item(desk, 20, 50)); // 20 desks for 50$
+            string actual = basket.statement();
+            string expected = "Счет для Chumakoff\n\tНазвание\tЦена\tКол-воСтоимость\tСкидка\tСумма\tБонус\n\tSchool Desk\t\t50\t20\t1000\t30\t970\t50\nСумма счета составляет 970\nВы заработали 50 бонусных балов";
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void bonusWithdraw()
+        {
+            pencil = new Goods("Cheap pencil", Goods.SPECIAL_OFFER);
+            student = new Customer("Chumakoff", 17);
+            basket = new Bill(student);
+            basket.addGoods(new Item(desk, 20, 50)); // 20 desks for 50$
+            string actual = basket.statement();
+            string expected = "Счет для Chumakoff\n\tНазвание\tЦена\tКол-воСтоимость\tСкидка\tСумма\tБонус\n\tSchool Desk\t\t50\t20\t1000\t47\t953\t50\nСумма счета составляет 953\nВы заработали 50 бонусных балов";
+            Assert.AreEqual(actual, expected);
         }
 
     }
